ff9cc2fa35400350d14528517c1f291dea800755
MilenaRPotes <110207483+MilenaRPotes@users.noreply.github.com>
2025-06-18 15:50:57 -0500

    Set up SQLite with Entity Framework Core and created initial Expenses table

---
+++ MoneyTracker/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoneyTracerk.Models;
+
+
+namespace MoneyTracker.Data
+{
+    class AppDbContext: DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           //Local SQLite file in the same folder as the application
+            optionsBuilder.UseSqlite("Data Source=moneytracker.db");
+        }
+    }
+}
---
+++ MoneyTracker/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTracker.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) 
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Add properties for binding here 
+    }
+}
